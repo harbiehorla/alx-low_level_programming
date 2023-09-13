5eb62fdccac5e533bcfa7b231c40e1b002b15060
harbiehorla <harbiehorla@gmail.com>
2023-09-12 17:01:52 -0700

    pur

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,24 @@
-#include "main.h"
-
-int main(void)
-{
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-
-    return (0);
-}
+#include <stdio.h> 
+ #include "main.h" 
+  
+ /** 
+  *   * main - Thid is the entry point of the program 
+  *     * This program, prints _putchar, followed by a new line. 
+  *       * 
+  *         * Return: Always 0 (indicating successful) 
+  *           */ 
+  
+ int main(void) 
+	 { 
+	          putchar('_'); 
+	          putchar('p'); 
+	          putchar('u'); 
+	          putchar('t'); 
+	          putchar('c'); 
+	          putchar('h'); 
+	          putchar('a'); 
+	          putchar('r'); 
+	          putchar('\n'); 
+	   
+	          return (0); 
+	  }}
