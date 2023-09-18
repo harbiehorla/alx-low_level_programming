b7c2e3c0c24bdf20331a70547f674398a0e24abf
harbiehorla <harbiehorla@gmail.com>
2023-09-18 15:01:57 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - prototype function to call the value of n
+ *
+ * @n: resets value of n to 98
+ *
+ * Return: retuns Null, to terminate function
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
