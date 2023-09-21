b6d685b545270365959e104473714f6a05861aa0
harbiehorla <harbiehorla@gmail.com>
2023-09-21 10:57:54 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints integars
+ *
+ * condition - solves n = 0, then n < 0, before converting to sting
+ *
+ * @n: declares integer to be printed using _putchar
+ *
+ * Return: returns void, to terminate function
+ */
+
+void print_number(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if ((n / 10) != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
+}
