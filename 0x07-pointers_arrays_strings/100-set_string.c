74b1887d33daf1118f03d1534f43c8b70841d397
harbiehorla <harbiehorla@gmail.com>
2023-09-25 14:56:36 -0700

    readme

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_string - prototype functions to set pointer to char
+ *
+ * @s: variable to double pointer
+ *
+ * @to: variable for chararacters
+ *
+ * Return: returns void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
