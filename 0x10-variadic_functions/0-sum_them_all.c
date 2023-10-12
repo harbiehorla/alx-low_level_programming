0885a4aaf77116fb1eed7a5a9c2cfa789c6264d6
harbiehorla <harbiehorla@gmail.com>
2023-10-12 13:59:04 -0700

    error fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,39 +1,29 @@
+/*
+ * File: 0-sum_them_all.c
+ */
+
 #include "variadic_functions.h"
 #include <stdarg.h>
 
 /**
- * sum_them_all - prototype function that sums a number of integars
+ * sum_them_all - Returns the sum of all its paramters.
+ * @n: The number of paramters passed to the function.
+ * @...: A variable number of paramters to calculate the sum of.
  *
- * condition - checks if n, before iterating the loop to checks each
- *		integar argument from va_list to calculate sum variables
- *
- * @n: variable specifying the number of arguments.
- *
- * Return: The sum of the integer arguments, or 0 if n is 0.
+ * Return: If n == 0 - 0.
+ *         Otherwise - the sum of all parameters.
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum;
-	unsigned int i, value;
-	va_list args;
+	va_list nums;
+	unsigned int index, sum = 0;
 
-	va_start(args, n);
-	if (n == 0)
-	{
-		return (0);
-	}
-	if (args == 0)
-	{
-		return (0);
-	}
+	va_start(nums, n);
 
-	sum = 0;
-	for (i = 0; i < n; i++)
-	{
-		value = va_arg(args, int);
-		sum = sum + value;
-	}
-	va_end(args);
+	for (index = 0; index < n; index++)
+		sum += va_arg(nums, int);
+
+	va_end(nums);
+
 	return (sum);
 }
