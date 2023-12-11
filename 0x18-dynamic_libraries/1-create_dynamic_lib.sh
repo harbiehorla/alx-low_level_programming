cf5e0934f11b94d27e96e59c802303f9a49676c9
harbiehorla <harbiehorla@gmail.com>
2023-12-11 14:02:38 -0800

    1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,2 @@
-#!bin/bash
-gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
-gcc -shared -o liball.so *.o
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+#!/bin/bash
+gcc -o *.c -fpic && gcc *o -shared -o liball.so 
